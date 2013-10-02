babc62ba1efd77c1d56f5dd1de5483d16e8eaeed
Administrator <user@example.com>
2013-10-02 16:32:38 +0900

    TDD 적용 -- 테스트 코드 프로젝트에 추가 코드와 테스트 코드 분리하여 관리

--- src/kr/co/zungwon/tunnel/core/Tunnel.java
+++ src/kr/co/zungwon/tunnel/core/Tunnel.java
@@ -2,13 +2,21 @@ package kr.co.zungwon.tunnel.core;
 
 public class Tunnel {
 
+	// TODO 원본포트 지정
+			public static final int srcPort = 1552;
+			public static final int dstPort = 1521;
+			
+			public static final String dstHost = "172.16.100.76";
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		System.out.println("Tunneling start");
-
+		// TODO 소케서버를 생성
+		
+		System.out.println("Tunneling teminate");
 	}
 
 }
