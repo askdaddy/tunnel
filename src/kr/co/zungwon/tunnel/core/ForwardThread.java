c6813af08d458349a4ff49ac0a79e9afd0761f78
Administrator <user@example.com>
2013-10-02 17:31:17 +0900

    포워딩 스레드 - 스프림의 전달을 담당한다.

---
+++ src/kr/co/zungwon/tunnel/core/ForwardThread.java
@@ -0,0 +1,11 @@
+package kr.co.zungwon.tunnel.core;
+
+/**  
+ * ForwardThread는 입력스트림과 출력스트림 사이에 TCP 포워딩을 담당한다.
+ * 입력된 스트림을 읽고 아웃풋 스트림으로 모두 포워딩한다.
+ * 스프림이 실패하면, 포워딩을 중단하고, 부모에게 그 소켓을 모두 닫기를 위해 알린다. 
+ */ 
+
+public class ForwardThread {
+
+}
