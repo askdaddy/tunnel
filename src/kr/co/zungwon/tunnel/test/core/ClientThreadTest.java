cde5f854b41d20f5765f7a51ddd56bbd7ae64cc0
Administrator <user@example.com>
2013-10-10 15:18:52 +0900

    클라이언트 스레드 생성 테스트 코드 작성

--- src/kr/co/zungwon/tunnel/test/core/ClientThreadTest.java
+++ src/kr/co/zungwon/tunnel/test/core/ClientThreadTest.java
@@ -1,5 +1,19 @@
 package kr.co.zungwon.tunnel.test.core;
 
+import org.junit.Test;
+import kr.co.zungwon.tunnel.core.ClientThread;
+import java.net.*;
+
 public class ClientThreadTest {
+	
+	@Test
+	public void ClientTreadCreate() throws Exception {
+		Socket clientSocket = null;
+		ClientThread clientTreadTest = new ClientThread(clientSocket);
+		if(clientTreadTest == null) {
+			throw new Exception("클라이언트  실패");
+		}
+		
+	}
 
 }
