0db1208f14d01d08015669bca9be702ed4058273
Administrator <user@example.com>
2013-10-11 18:44:47 +0900

    testFilter

---
+++ src/kr/co/zungwon/tunnel/test/core/FilterTest.java
@@ -0,0 +1,18 @@
+package kr.co.zungwon.tunnel.test.core;
+
+import org.junit.Test;
+import kr.co.zungwon.tunnel.core.Filter;
+
+public class FilterTest {
+	
+	@Test
+	public void FilterTest() throws Exception {
+		Filter filterTest = new Filter("string");
+		
+		if(filterTest == null) {
+			throw new Exception("파일생성실패");
+		}
+		
+	}
+
+}
