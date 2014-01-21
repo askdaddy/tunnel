b4ac374780d76059da2b3d25330af274826a03dc
DURU <enfn2001@gmail.com>
2014-01-21 18:01:42 +0900

    MSTSC port TEST

--- src/kr/co/zungwon/tunnel/core/Tunnel.java
+++ src/kr/co/zungwon/tunnel/core/Tunnel.java
@@ -4,14 +4,13 @@ package kr.co.zungwon.tunnel.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-// TODO 참조한 코드 기재
 
 public class Tunnel {
 
-	// TODO 원본포트 지정
-			public static final int srcPort = 1552;
-			public static final int dstPort = 1521;
-			public static final String dstHost = "172.16.100.76";
+			public static final int srcPort = 8888;
+			public static final int dstPort = 3389;
+			public static final String dstHost = "211.201.139.10";
+			//public static final String dstHost = "movie.naver.com";
 	/**
 	 * @param args
 	 */
@@ -19,20 +18,18 @@ public class Tunnel {
 		
 		
 		System.out.println("Tunneling start");
-		// TODO 소케서버를 생성
 		ServerSocket svrSOCK = new ServerSocket(srcPort);
 		
-		
+		//svrSOCK.close();
 		while(true){
-			// TODO 클라이언트 소켓 생성
 			Socket clientSocket = svrSOCK.accept();
-		
-			// TODO 클라이언트쓰레드 생성
+
 			ClientThread clientThread = new ClientThread(clientSocket);
-			
-			// TODO 자식스레드 생성
+
 			clientThread.start();
 		}
+		
+		
 
 	}
 
